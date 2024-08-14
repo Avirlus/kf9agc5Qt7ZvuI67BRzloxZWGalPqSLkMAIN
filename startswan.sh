0890367b7334117d322f0542699248f38bb44903
Avirlus <118074221+Avirlus@users.noreply.github.com>
2024-08-14 19:06:56 +0300

    Update startswan.sh

--- startswan.sh
+++ startswan.sh
@@ -4,7 +4,7 @@ myip=$(hostname -I | awk '{print $1}')
 apt install strongswan strongswan-pki libcharon-extra-plugins libcharon-extauth-plugins -y
 
 # Создаем резервную копию файла настроек
-mv /etc/ipsec.conf{,.original} 
+mv /etc/ipsec.conf{,.original}
 
 # Настраиваем конфигурацию IPsec для использования PSK
 cat << EOF > /etc/ipsec.conf
@@ -23,7 +23,6 @@ conn ikev2-vpn
     dpddelay=300s
     rekey=no
     left=%any
-    leftid=$myip
     authby=secret
     leftsubnet=0.0.0.0/0
     right=%any
@@ -41,99 +40,28 @@ EOF
 cat << EOF > /etc/ipsec.secrets
 # Формат: leftid : PSK "shared_secret"
 $myip : PSK "vpn37300593000"
-# your_username : EAP "your_password" - используйте этот формат для добавления нового пользователя
 EOF
 
 ufw allow OpenSSH
 ufw enable
-ufw allow 500,4500/udp
+ufw allow 500/udp
+ufw allow 4500/udp
 
+# Замените ens192 на ваш сетевой интерфейс
+interface=$(ip -4 route ls | grep default | grep -Po '(?<=dev )(\S+)')
 cat << EOF > /etc/ufw/before.rules
 *nat
-#change ens192 interface to yours
--A POSTROUTING -s 10.10.10.0/24 -o ens192 -m policy --pol ipsec --dir out -j ACCEPT
--A POSTROUTING -s 10.10.10.0/24 -o ens192 -j MASQUERADE                             
+-A POSTROUTING -s 10.10.10.0/24 -o $interface -m policy --pol ipsec --dir out -j ACCEPT
+-A POSTROUTING -s 10.10.10.0/24 -o $interface -j MASQUERADE                             
 COMMIT
 
 *mangle
-#change ens192 interface to yours
--A FORWARD --match policy --pol ipsec --dir in -s 10.10.10.0/24 -o ens192 -p tcp -m tcp --tcp-flags SYN,RST SYN -m tcpmss --mss 1361:1536 -j TCPMSS --set-mss 1360
+-A FORWARD --match policy --pol ipsec --dir in -s 10.10.10.0/24 -o $interface -p tcp -m tcp --tcp-flags SYN,RST SYN -m tcpmss --mss 1361:1536 -j TCPMSS --set-mss 1360
 COMMIT
-#
-# rules.before
-#
-# Rules that should be run before the ufw command line added rules. Custom
-# rules should be added to one of these chains:
-#   ufw-before-input
-#   ufw-before-output
-#   ufw-before-forward
-#
 
-# Don't delete these required lines, otherwise there will be errors
 *filter
-:ufw-before-input - [0:0]
-:ufw-before-output - [0:0]
-:ufw-before-forward - [0:0]
-:ufw-not-local - [0:0]
-# End required lines
 -A ufw-before-forward --match policy --pol ipsec --dir in --proto esp -s 10.10.10.0/24 -j ACCEPT
 -A ufw-before-forward --match policy --pol ipsec --dir out --proto esp -d 10.10.10.0/24 -j ACCEPT
-
-# allow all on loopback
--A ufw-before-input -i lo -j ACCEPT
--A ufw-before-output -o lo -j ACCEPT
-
-# quickly process packets for which we already have a connection
--A ufw-before-input -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
--A ufw-before-output -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
--A ufw-before-forward -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
-
-# drop INVALID packets (logs these in loglevel medium and higher)
--A ufw-before-input -m conntrack --ctstate INVALID -j ufw-logging-deny
--A ufw-before-input -m conntrack --ctstate INVALID -j DROP
-
-# ok icmp codes for INPUT
--A ufw-before-input -p icmp --icmp-type destination-unreachable -j ACCEPT
--A ufw-before-input -p icmp --icmp-type time-exceeded -j ACCEPT
--A ufw-before-input -p icmp --icmp-type parameter-problem -j ACCEPT
--A ufw-before-input -p icmp --icmp-type echo-request -j ACCEPT
-
-# ok icmp code for FORWARD
--A ufw-before-forward -p icmp --icmp-type destination-unreachable -j ACCEPT
--A ufw-before-forward -p icmp --icmp-type time-exceeded -j ACCEPT
--A ufw-before-forward -p icmp --icmp-type parameter-problem -j ACCEPT
--A ufw-before-forward -p icmp --icmp-type echo-request -j ACCEPT
-
-# allow dhcp client to work
--A ufw-before-input -p udp --sport 67 --dport 68 -j ACCEPT
-
-#
-# ufw-not-local
-#
--A ufw-before-input -j ufw-not-local
-
-# if LOCAL, RETURN
--A ufw-not-local -m addrtype --dst-type LOCAL -j RETURN
-
-# if MULTICAST, RETURN
--A ufw-not-local -m addrtype --dst-type MULTICAST -j RETURN
-
-# if BROADCAST, RETURN
--A ufw-not-local -m addrtype --dst-type BROADCAST -j RETURN
-
-# all other non-local packets are dropped
--A ufw-not-local -m limit --limit 3/min --limit-burst 10 -j ufw-logging-deny
--A ufw-not-local -j DROP
-
-# allow MULTICAST mDNS for service discovery (be sure the MULTICAST line above
-# is uncommented)
--A ufw-before-input -p udp -d 224.0.0.251 --dport 5353 -j ACCEPT
-
-# allow MULTICAST UPnP for service discovery (be sure the MULTICAST line above
-# is uncommented)
--A ufw-before-input -p udp -d 239.255.255.250 --dport 1900 -j ACCEPT
-
-# don't delete the 'COMMIT' line or these rules won't be processed
 COMMIT
 EOF
 
