74c1335984875aac4c851651c6828ebbded265b7
Avirlus <118074221+Avirlus@users.noreply.github.com>
2024-08-14 15:15:26 +0300

    Update startswan.sh

--- startswan.sh
+++ startswan.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt update
-myip=$(wget -qO - eth0.me)
+myip=$(wget -qO - ens192.me)
 apt install strongswan strongswan-pki libcharon-extra-plugins libcharon-extauth-plugins -y
 
 # Создаем резервную копию файла настроек
@@ -50,14 +50,14 @@ ufw allow 500,4500/udp
 
 cat << EOF > /etc/ufw/before.rules
 *nat
-#change eth0 interface to yours
--A POSTROUTING -s 10.10.10.0/24 -o eth0 -m policy --pol ipsec --dir out -j ACCEPT
--A POSTROUTING -s 10.10.10.0/24 -o eth0 -j MASQUERADE                             
+#change ens192 interface to yours
+-A POSTROUTING -s 10.10.10.0/24 -o ens192 -m policy --pol ipsec --dir out -j ACCEPT
+-A POSTROUTING -s 10.10.10.0/24 -o ens192 -j MASQUERADE                             
 COMMIT
 
 *mangle
-#change eth0 interface to yours
--A FORWARD --match policy --pol ipsec --dir in -s 10.10.10.0/24 -o eth0 -p tcp -m tcp --tcp-flags SYN,RST SYN -m tcpmss --mss 1361:1536 -j TCPMSS --set-mss 1360
+#change ens192 interface to yours
+-A FORWARD --match policy --pol ipsec --dir in -s 10.10.10.0/24 -o ens192 -p tcp -m tcp --tcp-flags SYN,RST SYN -m tcpmss --mss 1361:1536 -j TCPMSS --set-mss 1360
 COMMIT
 #
 # rules.before
